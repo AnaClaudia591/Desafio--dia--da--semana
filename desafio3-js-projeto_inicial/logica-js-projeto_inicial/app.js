1e1c1cc4668f3cb10d4f31ef719522424fd610a6
AnaClaudia591 <ana.claudiabentonasc@gmail.com>
2024-12-15 15:43:12 -0300

    Desafio do curso Alura

---
+++ desafio3-js-projeto_inicial/logica-js-projeto_inicial/app.js
@@ -0,0 +1,12 @@
+alert ('Boas vindas ao desafio da semana1');
+
+// 1. Pergunte ao usuário qual é o dia da semana.
+let diaSemana = prompt("Qual é o dia da semana?");
+
+if (diaSemana === "Sábado" || diaSemana === "Domingo") {
+
+    alert("Bom fim de semana!");
+} else {
+
+    alert("Boa semana!");
+}
